c1d8bcac9b9502fae6af309378faa35e09ca80a5
Eternum101 <eternumhd111@gmail.com>
2024-01-23 18:09:26 +1000

    Added AdvertWidget.jsx & FriendListWidget.jsx Added Friend into Initial State in index.js in State Folder Modified Layout in index.jsx in homePage Folder Modified Colours in theme.js

---
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,37 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+    const { palette } = useTheme();
+    const dark = palette.neutral.dark;
+    const main = palette.neutral.main;
+    const medium = palette.neutral.medium;
+
+    return (
+        <WidgetWrapper>
+            <FlexBetween>
+                <Typography color={dark} variant="h5" fontWeight="500">
+                    Sponsored
+                </Typography>
+                <Typography color={medium}>
+                    Create Ad
+                </Typography>
+            </FlexBetween>
+            <img width="100%" height="auto" alt="advert" src="/assets/info4.jpeg" style={{ borderRadius: "0.75rem", margin:"0.75rem 0"}}/>
+            <FlexBetween>
+                <Typography color={main}>
+                    Biobeauty
+                </Typography>
+                <Typography color={medium}>
+                    biobeauty.com
+                </Typography>
+            </FlexBetween>
+            <Typography color={medium} margin="0.5rem 0">
+                Our carefully curated portfolio of exclusive professional brands empower our network of over 2,000 salon customers to offer world-leading products and beauty services ... without compromise.
+            </Typography>
+        </WidgetWrapper>
+    )
+}
+
+export default AdvertWidget;
