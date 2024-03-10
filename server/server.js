b9738cb3eea73da3da07facafd876c095b15f9f1
Eternum101 <eternumhd111@gmail.com>
2024-03-11 09:55:21 +1000

    Removed Catch-All Route Handler in server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,6 @@ app.use(cors({
     origin: ["http://localhost:3000", 
     "https://social-media-app-el95.onrender.com"],
 }));
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 const storage = multer.diskStorage( {
     destination: function (req, file, cb) {
@@ -49,11 +48,7 @@ app.patch("/users/:id/updateProfilePicture", verifyToken, upload.single('picture
 
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-app.use("/posts", postRoutes);
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+app.use("/posts", postRoutes); 
 
 const PORT = process.env.PORT || 5001;
 mongoose.connect(process.env.MONGO_URL, {
